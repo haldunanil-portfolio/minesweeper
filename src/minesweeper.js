0a32670dbde8ef91486b56da76e3c0da625973d5
Haldun Anil <haldunanil@gmail.com>
2018-03-19 18:22:21 -0400

    Dyanmically generate boards
    Accept row and column counts to generate player and bomb boards for the game

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,19 +1,53 @@
-const printBoard = (board) => {
-	console.log('Current Board:');
-	console.log(board[0].join(' | '));
-	console.log(board[1].join(' | '));
-	console.log(board[2].join(' | '));
+const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
+	let board = [];
+	for (let row = 0; row < numberOfRows; row++) {
+		let row = [];
+		for (let col = 0; col < numberOfColumns; col++) {
+			row.push(' ');
+		}
+		board.push(row);
+	}
+	return board;
 };
 
-const board = [
-	[' ', ' ', ' '], 
-	[' ', ' ', ' '], 
-	[' ', ' ', ' ']
-];
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+	let board = [];
+	for (let row = 0; row < numberOfRows; row++) {
+		let row = [];
+		for (let col = 0; col < numberOfColumns; col++) {
+			row.push(null);
+		}
+		board.push(row);
+	}
 
-printBoard(board);
-board[0][1] = '1';
-board[2][2] = 'B';
+	// place bombs, keep going until desired number of bombs are placed
+	let numberOfBombsPlaced = 0;  // keep track of bombs placed on the board
+	while (numberOfBombsPlaced < numberOfBombs) {
+		// todo: need to fix overlapping bombs
 
+		let randomRowIndex = Math.floor(Math.random() * numberOfRows);  // randomly pick a row on the board to add a bomb to
+		let randomColIndex = Math.floor(Math.random() * numberOfColumns);  // randomly pick a column on the board to add a bomb to
+		board[randomRowIndex][randomColIndex] = 'B';  // place a bomb
+		numberOfBombsPlaced++;  // increment
+	}
 
-printBoard(board);
+	// return the bomb board
+	return board;	
+};
+
+const printBoard = (board) => {
+	console.log(board.map(row => row.join(' | ')).join('\n'));
+}
+
+// generate a player board
+let playerBoard = generatePlayerBoard(3, 4);
+
+// generate a bomb board
+let bombBoard = generateBombBoard(3, 4, 5);
+
+// print both boards to console
+console.log('Player Board: ')
+printBoard(playerBoard);
+
+console.log('Bomb Board: ')
+printBoard(bombBoard);
