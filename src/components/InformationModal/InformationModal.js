0791d23198c1aec0ba06b875d4d5e9f8fc75df7f
Haldun Anil <haldunanil@gmail.com>
2018-04-10 18:11:23 -0400

    Add "how to play" section

---
+++ src/components/InformationModal/InformationModal.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import Modal from "react-modal";
+import "./InformationModal.css";
+
+class InformationModal extends Component {
+  componentWillMount() {
+    Modal.setAppElement("body");
+  }
+
+  render() {
+    return (
+      <Modal
+        id="Information-modal"
+        isOpen={this.props.isOpen}
+        shouldCloseOnOverlayClick={true}
+        onRequestClose={() => {
+          this.props.handleClose();
+        }}
+      >
+        <h2>How to Play</h2>
+        <p>1) Select the level of difficulty you want.</p>
+        <p>2) Click or tap on any tile to get started.</p>
+        <p>3) If a tile has a bomb, you lose. :(</p>
+        <p>
+          4) If a tile shows a number, that tells you how many bombs are around
+          that tile (hint: there can be up to 8 bombs around any tile).
+        </p>
+        <p>
+          5) A tile with a number is safe! Keep on trying to find these without
+          blowing up.
+        </p>
+        <p>
+          6) If you find all the tiles without exploding, you win! As an added
+          challenge, try to finish faster.
+        </p>
+        <p>7) Enjoy! :) </p>
+
+        <button className="Modal-screen-close" onClick={this.props.handleClose}>
+          CLOSE
+        </button>
+      </Modal>
+    );
+  }
+}
+
+export default InformationModal;
